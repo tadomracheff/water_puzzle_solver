2d410d5156b55046b7ecb05e49f6716316bd039f
tadomracheff <tadomracheff@gmail.com>
2021-04-21 22:51:26 +0500

    add check for extra moves

--- classes/Flask.py
+++ classes/Flask.py
@@ -28,9 +28,13 @@ class Flask:
 
     def is_assembled(self):
         if self.get_size() == self.capacity:
-            if [self.value[0]] * self.get_size() == self.value:
-                return True
+            return self.is_one_color()
         return False
+
+    def is_one_color(self):
+        if self.is_empty():
+            return False
+        return [self.value[0]] * self.get_size() == self.value
 
     def get_size(self):
         return len(self.value)
@@ -44,14 +48,24 @@ class Flask:
     def is_full(self):
         return self.get_size() == self.capacity
 
-    def move_possible(self, flask):
-        if self.is_empty():
+    ###############
+    # колба-источник не должна быть пустой и полностью собранной
+    # если колба-источник заполнена одинаковыми цветами, нельзя переливать ее в пустую колбу
+    # если обе колбы заполнены одинаковыми цветами, нужно перелить из меньшей в большую
+    ###############
+    def move_possible(self, flask_to):
+        if self.is_empty() or self.is_assembled():
             return False
-        if flask.is_empty():
+        if flask_to.is_full():
+            return False
+        if self.is_one_color() and flask_to.is_empty():
+            return False
+        if self.is_one_color() and flask_to.is_one_color() and self.get_size() > flask_to.get_size():
+            return False
+        if flask_to.is_empty():
             return True
-        if flask.is_full():
-            return False
-        return self.get_top() == flask.get_top()
+
+        return self.get_top() == flask_to.get_top()
 
     def move(self, flask):
         if not flask.is_full():
