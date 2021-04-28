450c88a7937ca4fbd6d67409ab29692c9a306362
tadomracheff <tadomracheff@gmail.com>
2021-04-28 19:29:10 +0500

    adв priority of moves to non-empty flasks

--- main.py
+++ main.py
@@ -107,11 +107,19 @@ def get_moves(state):
     if is_win(state):
         return moves
 
-    for flask1 in state:
-        for flask2 in state:
-            if flask1.order != flask2.order and flask2.move_possible(flask1):
-                move = Move(flask2.order, flask1.order)
+    for flask_from in state:
+        for flask_to in state:
+            if flask_to.order != flask_from.order and not flask_to.is_empty() and flask_from.move_possible(flask_to):
+                move = Move(flask_from.order, flask_to.order)
                 moves.append(move)
+
+    if moves.__eq__([]):
+        for flask_from in state:
+            for flask_to in state:
+                if flask_to.order != flask_from.order and flask_from.move_possible(
+                        flask_to):
+                    move = Move(flask_from.order, flask_to.order)
+                    moves.append(move)
     return moves
 
 
@@ -205,10 +213,10 @@ def game_init(flasks_init, empty_count, capacity):
 if __name__ == '__main__':
     # flasks = game_init([[1, 2], [2, 1]], 1, 2)
     # flasks = game_init([[1, 2, 3], [2, 1, 3], [2, 1, 3]], 2, 3)
-    # flasks = game_init([[1, 2, 3, 1], [1, 2, 3, 3], [2, 3, 1, 2]], 2, 4)
+    # flasks = game_init([[1, 1, 1, 2], [3, 4, 1, 3], [3, 2, 2, 3], [2, 4, 4, 4]], 2, 4)
     # flasks = game_init([[4,12,2,3],[1,9,5,7],[5,11,4,9],[3,11,1,10],[5,8,10,9],[5,12,9,3],[2,12,7,6],[1,6,7,11],[8,8,7,4],[6,10,11,2],[12,4,3,2],[1,10,6,8]], 2, 4)
-    # flasks = game_init([[1,2,2,3],[4,2,5,6],[7,8,8,8],[3,7,9,4],[10,1,6,5],[3,9,11,1],[11,12,8,1],[4,12,9,10],[11,4,3,6],[5,2,5,12],[7,7,6,10],[11,12,10,9]], 2, 4)
-    flasks = game_init([[1, 2, 3, 4], [2, 1, 3, 4], [2, 1, 3, 4], [4, 2, 1, 3]], 2, 4)
+    flasks = game_init([[1,2,2,3],[4,2,5,6],[7,8,8,8],[3,7,9,4],[10,1,6,5],[3,9,11,1],[11,12,8,1],[4,12,9,10],[11,4,3,6],[5,2,5,12],[7,7,6,10],[11,12,10,9]], 2, 4)
+    # flasks = game_init([[1, 2, 3, 4], [2, 1, 3, 4], [2, 1, 3, 4], [4, 2, 1, 3]], 2, 4)
     # flasks = game_init([[1, 1, 1, 2], [1, 3, 2, 4], [4, 2, 4, 3], [3, 4, 3, 5], [2, 5, 5, 5]], 2, 4)
 
     files = glob.glob('solution/*')
